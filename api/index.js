7605baed9f6934b3ee8daff99b47b75f5cdd2aef
Aniket <aniketakasavan2001@gmail.com>
2024-11-03 08:03:45 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
+
+
 mongoose.connect(process.env.MONGO).then(() =>{
     console.log("connected to mongodb!");
 })
@@ -10,6 +13,15 @@ mongoose.connect(process.env.MONGO).then(() =>{
 })
 const app = express();
 
+app.use(express.json());
+//test api
+// app.get("/test",(req,res) =>{
+//     res.json()
+// })
+app.use('/api/auth',authRouter);
+
+
 app.listen(3000 , () =>{
     console.log("sever is running on localhost:3000!");
 });
+
